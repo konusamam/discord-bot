2912f2b599bff4e7fd9bcb33fe6bd1e1cacc4d0e
Chao Cheng <chaozcheng@gmail.com>
2019-04-15 11:45:20 -0400

    Alter response determination order

--- events/message.js
+++ events/message.js
@@ -1,40 +1,36 @@
 module.exports = (client, msg) => {
-	
 	if (msg.author.bot) {  // Ignore own messages
 		return
 	}
 
 	let text = msg.content.toLowerCase()
 
-	let dogical_words = ["d0g", "d0ge", "dogical", "dogger", "dorg", "dogue", "dorq"]
-	for (let word of dogical_words) {
-		if (text.includes(word)) {
-			msg.channel.send("Doge!!🐕🐕🐕")
-			return
-		}
-	}
-
 	if (text.includes("doge")) {
+		let doge_reply = "Doge"
+		let doge_emojis = ["🐶", "🐨", "🐼", "🦊", "🐻", "🦁", "🐮", "🐷", "🐸", "❤️", "💙", "💜", "💛", "💚", "💖"]
 		let num_doges = text.split("doge").length - 1
-		let dogical_reply = "Doge"
+		
 		for (let i = 1; i < num_doges; i++) {
-			dogical_reply += "doge"
+			doge_reply += "doge"
 		}
-		dogical_reply += "!!!!!!!"
-		let dogical_emojis = ["🐶", "🐨", "🐼", "🦊", "🐱", "❤️", "💙", "💜", "🧡", "💛", "💚", "💖"]
+		doge_reply += "!!!!!!!"
+
 		for (let i = 0; i < (Math.floor(Math.random() * 12) + 5); i++) {
-			dogical_reply += dogical_emojis[Math.floor(Math.random() * 12)]
+			doge_reply += doge_emojis[Math.floor(Math.random() * doge_emojis.length)]
 		}
-		msg.channel.send(dogical_reply)
+
+		msg.channel.send(doge_reply)
 	} else if (text == "dog") {
 		msg.channel.send("I am no mere dog. I am DOGE.")
+	} else if (text.includes("d0g") || text.includes("d0ge")) {
+		msg.channel.send("Doge!!🐕🐕🐕")
 	} else if (text == "help") {
 		msg.channel.send("woof woof! I'm a doge.")
-	} else if (text == "doggystyle") {
-		msg.channel.send("OwO what's this? 😳❓Big red ❤💋meaty steak 🥩🍖 UwU ❤❤ 💖Mmm~ So tasty yummy UwO 😉😘, *licks meat*👅👅 💓💓Unnf UwU tastesss soo gwoood daddy ☺🤩😜~~ What?! ⁉️ EGGS?!?🥚⁉️ :0 😮 UwU *Shakeys wittle baby tail*🐶💖❤ mmm Daddy I Wuuuuv eggs~ 🥚💋💖I Wuuuuuuuv💗 💞 Yo Eggs espweciawy uwo, mmm tastes soooo good~ 💦🤤💙Daddy these eggs are sawty~💖💖 OwO ❤😍 daddy is that a sausage to add to my sawty eggy weggys?💖 🤤🥚💗 Mmm *licks sausage* 👅💦 mmmm soooo good~ 🤤💖🥴 *deepthroatys daddies big thick juicy meat* 😳👅 mmmm daddy this tastes SOOOOO GOOOOOD~ 😜💦💜MMMM OwO UwU~ *Nuzzles you*💗 RAWR 🐯🐻🦁💞")
-	} else if (text.includes("wtf")) {
+	} else if (text.includes("what") || text.includes("wot") || text.includes("w0t")) {
 		msg.channel.send("dooooooooooooooge", {
 			file: "./images/doge.gif"
 		})
+	} else if (text == "doggystyle") {
+		msg.channel.send("OwO what's this? 😳❓Big red ❤💋meaty steak 🥩🍖 UwU ❤❤ 💖Mmm~ So tasty yummy UwO 😉😘, *licks meat*👅👅 💓💓Unnf UwU tastesss soo gwoood daddy ☺🤩😜~~ What?! ⁉️ EGGS?!?🥚⁉️ :0 😮 UwU *Shakeys wittle baby tail*🐶💖❤ mmm Daddy I Wuuuuv eggs~ 🥚💋💖I Wuuuuuuuv💗 💞 Yo Eggs espweciawy uwo, mmm tastes soooo good~ 💦🤤💙Daddy these eggs are sawty~💖💖 OwO ❤😍 daddy is that a sausage to add to my sawty eggy weggys?💖 🤤🥚💗 Mmm *licks sausage* 👅💦 mmmm soooo good~ 🤤💖🥴 *deepthroatys daddies big thick juicy meat* 😳👅 mmmm daddy this tastes SOOOOO GOOOOOD~ 😜💦💜MMMM OwO UwU~ *Nuzzles you*💗 RAWR 🐯🐻🦁💞")
 	}
 }
