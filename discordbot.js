ca88ead00bf96c7f1568f4284df7044ec605dd5a
Chao Cheng <chaozcheng@gmail.com>
2019-05-05 18:11:05 -0400

    Use dotenv

--- discordbot.js
+++ discordbot.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js")
 const fs = require("fs")
+const dotenv = require("dotenv")
+dotenv.config()
 
 const client = new Discord.Client()
 
@@ -11,4 +13,4 @@ fs.readdir("./events/", (err, files) => {
 	})
 })
 
-client.login(DISCORD_TOKEN)
+client.login(process.env.TOKEN)
